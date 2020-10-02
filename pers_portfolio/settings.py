0024f296f3d4e468bb4995bfe5524d28201fe73e
greenhat1001 <emanresuwen@protonmail.com>
2020-10-02 13:08:24 +0000

    Remove old stuff

--- pers_portfolio/settings.py
+++ pers_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k*fk5^@8k--g-$n-(lp4)r(*=*!-5*m(#8j@a^4!t%38^_f)7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yellowhat0110.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
